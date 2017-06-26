5a87d7458990e10888be2b378ed4dd3253486a74
Hezser <contact.sergiohernandez@gmail.com>
2017-06-26 20:46:54 +0100

    Initial Commit

---
+++ Trit/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Trit
+//
+//  Created by Sergio Hernandez on 26/06/2017.
+//  Copyright © 2017 Sergio Hernandez. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Trit/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Trit
+//
+//  Created by Sergio Hernandez on 26/06/2017.
+//  Copyright © 2017 Sergio Hernandez. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
